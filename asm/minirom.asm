6698c458833d09dd513fd3449cc4c6a2d49ffb2c
Daniel Serpell <daniel.serpell@gmail.com>
2018-11-29 00:31:51 -0300

    Optimize a few bytes from minirom, blink leds on input.

--- asm/minirom.asm
+++ asm/minirom.asm
@@ -1,11 +1,27 @@
 
         opt     f+h-l+
 
-UARTD = $FE20
-UARTS = $FE21
-TIMERL = $FE00
-TIMERH = $FE01
-TIMERC = $FE02
+; TIMER registers:
+TIMERL  = $FE00   ; TIMER low counter (R/W)
+TIMERH  = $FE01   ; TIMER high coutner (R/W)
+TIMERC  = $FE02   ; TIMER status (R), control (W)
+
+; UART registers:
+UARTD   = $FE20   ; UART RX data (R) , TX data (W)
+UARTS   = $FE21   ; UART status (R) , clear flafs (W)
+
+; LED Driver registers:
+LEDDPWRR = $FE41   ; LED Driver Pulse Width Register for RED (W)
+LEDDPWRG = $FE42   ; LED Driver Pulse Width Register for GREEN (W)
+LEDDPWRB = $FE43   ; LED Driver Pulse Width Register for BLUE (W)
+
+LEDDBCRR = $FE45   ; LED Driver Breathe On Control Register (W)
+LEDDBCFR = $FE46   ; LED Driver Breathe Off Control Register (W)
+
+LEDDCR0  = $FE48   ; LED Driver Control Register 0 (W)
+LEDDBR   = $FE49   ; LED Driver Pre-scale Register (W)
+LEDDONR  = $FE4A   ; LED Driver ON Time Register (W)
+LEDDOFR  = $FE4B   ; LED Driver OFF Time Register (W)
 
 ptr     = 0   // Use locations 0,1 as pointer
 tmp     = 2
@@ -13,7 +29,91 @@ tmp     = 2
 
         org     $FF00
 
-print_hex:
+;----------------------------------------------------------------------
+; Reads an HEX number, exits to prompt on error
+get_hex .proc
+
+        jsr     again
+again:
+        jsr     get_char
+        eor     #'0'
+        cmp     #10
+        bcc     digit
+        ora     #$20            ; Lower to upper case
+        adc     #$88
+        cmp     #$FA
+        bcc     exit_prompt     ; Not an hex number
+digit:
+        sec
+        rol
+        asl
+        asl
+        asl
+        asl
+rloop:
+        rol     tmp
+        asl
+        bne     rloop
+        lda     tmp
+        rts
+
+    .endp
+
+
+;----------------------------------------------------------------------
+; Reads a character, ignores control chars and spaces, echoes the
+; character back. Exits to prompt on timeout.
+get_char .proc
+
+        ldx     #5              // Timeout: 2.5 seconds is app 250 * 60000 cycles
+
+        // Init timer to 24000-2
+t24000:
+        stx     LEDDPWRG        // Show LED interaction
+        lda     #$E9    // Optimization: use $E9E9 for timer reload, giving $E9EB cycles
+        sta     TIMERL
+        sta     TIMERH
+        lda     #1
+        sta     TIMERC  // Start timer
+
+        // Check if we need to keep waiting
+        inx
+        bne     wait
+
+        // Timeout, stop timer and go to prompt
+        stx     TIMERC
+.def :exit_prompt
+        pla
+        pla
+        jmp     prompt
+
+        // Wait for timer or uart receive
+wait:
+        lda     TIMERC
+        bmi     t24000
+        bit     UARTS
+        bvc     wait
+
+        dec     TIMERC  // Stop timer
+        // Ok, we have a character, return it
+        lda     UARTD
+        sta     UARTS
+        cmp     #'!'
+        bcc     get_char
+
+    .endp       ; Fall-through
+
+put_char .proc
+        bit     UARTS
+        bmi     put_char
+        sta     UARTD
+        rts
+    .endp       ; PROMPT bellow uses this "RTS" as '`':
+
+prompt_msg = *-1
+        .byte   10, 13
+
+print_hex .proc
         pha
         lsr
         lsr
@@ -27,35 +127,36 @@ hex_digit:
 	cmp	#$0A		; set carry for +1 if >9
 	adc	#'0'		; add ASCII "0"
 	cld			; clear decimal mode
-put_char:
-        bit     UARTS
-        bmi     put_char
-        sta     UARTD
-        rts
+        bne put_char
+    .endp
 
 reset:
         cld
         sei
 
         // Print initial character
-        lda     #'+'
+        lda     #'#'
         sta     UARTD
 
-        // Init stack pointer
-        ldx     #$FF
-        txs
-
         // Test two bytes of ZP RAM
-        lda     #'Z'
+        asl     ; Use A = $46 (F) to signal error
         sta     ptr
         cmp     ptr
         bne     put_char
 
+        asl     ; Use A = $8C to init LED control
+        sta     LEDDCR0
+
         // Now test and fill all memory with 0
-        dex             // $FE = Fill up to $FDFF
-        lda     #0
-        sta     ptr
-        sta     ptr+1
+        ldx     #0
+        stx     ptr
+        stx     ptr+1
+
+        // Init stack pointer
+        dex     // X = $FF
+        txs
+
+        dex     // X = $FE, Fill up to $FDFF
         ldy     #ptr+2  // From ptr+2
 
 clrmem
@@ -86,12 +187,12 @@ end_ram:
 
         // Prompt and process commands
 prompt:
-        lda     #13
+        ldx     #2
+prompt_loop:
+        lda     prompt_msg,x
         jsr     put_char
-        lda     #10
-        jsr     put_char
-        lda     #'>'
-        jsr     put_char
+        dex
+        bpl     prompt_loop
 
         // Get command address: 2 bytes
         jsr     get_hex
@@ -103,12 +204,6 @@ prompt:
         jsr     get_char
         ldy     #0
 
-        // "R" means "RUN"
-        cmp     #'R'
-        bne     not_run
-        jmp     (ptr)
-
-not_run:
         // ":" means "ENTER" 16 bytes
         cmp     #':'
         bne     not_enter
@@ -119,12 +214,12 @@ enter_loop:
         iny
         cpy     #$10
         bne     enter_loop
-        beq     prompt
+        tya     ; Fall through next comparison with A=10, so it is false.
 
 not_enter:
         // "S" means "SHOW" 16 bytes
-        cmp     #'S'
-        bne     prompt
+        eor     #'S'
+        bne     not_show
 
 show_loop:
         lda     (ptr), y
@@ -132,73 +227,14 @@ show_loop:
         iny
         cpy     #$10
         bne     show_loop
-        beq     prompt
+        tya     ; Fall through next comparison with A=10, so it is false.
 
-        // Wait for characters from serial port, print "." once each second
-get_char:
-        ldx     #5              // Timeout: 2.5 seconds is app 250 * 60000 cycles
+not_show:
+        // "R" means "RUN" - EOR above menas "R" == 1
+        lsr
+        bne     prompt
+        jmp     (ptr)
 
-        // Init timer to 24000-2
-t24000:
-        lda     #$E9    // Optimization: use $E9E9 for timer reload, giving $E9EB cycles
-        sta     TIMERL
-        sta     TIMERH
-        lda     #1
-        sta     TIMERC  // Start timer
-
-        // Check if we need to keep waiting
-        inx
-        bne     wait
-
-        // Timeout, stop timer and go to prompt
-        stx     TIMERC
-exit_prompt:
-        pla
-        pla
-        jmp     prompt
-
-        // Wait for timer or uart receive
-wait:
-        lda     TIMERC
-        bmi     t24000
-        bit     UARTS
-        bvc     wait
-
-        dec     TIMERC  // Stop timer
-        // Ok, we have a character, return it
-        lda     UARTD
-        sta     UARTS
-        cmp     #'!'
-        bcc     get_char
-        jsr     put_char
-        rts
-
-get_hex .proc
-
-        jsr     again
-again:
-        jsr     get_char
-        eor     #'0'
-        cmp     #10
-        bcc     digit
-        adc     #$88
-        cmp     #$FA
-        bcc     exit_prompt     ; Not an hex number
-digit:
-        sec
-        rol
-        asl
-        asl
-        asl
-        asl
-rloop:
-        rol     tmp
-        asl
-        bne     rloop
-        lda     tmp
-        rts
-
-        .endp
 
 nmi = $200
 irq = $203
